aefd48ad50ea816ba25f0baf56f0565f6ee08021
imyukehan <njyukehan@gmail.com>
2025-12-15 18:17:01 +0800

    Initial Commit

---
+++ Kazeyomi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kazeyomi
+//
+//  Created by 于可汗 on 12/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kazeyomi/KazeyomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KazeyomiApp.swift
+//  Kazeyomi
+//
+//  Created by 于可汗 on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct KazeyomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
